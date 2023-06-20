5c7d48f5d6c19d5a961cada1f15cd21763dbb19a
Mahbub <mahbubhussaincse@gmail.com>
2023-06-20 08:42:44 +0600

    PHP mailer in google

---
+++ mailer/index.php
@@ -0,0 +1,56 @@
+<?php 
+use App\Mahbub\Phpmailer\Config\Config;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+
+include_once __DIR__ .'/vendor/autoload.php';
+
+
+
+$mail = new PHPMailer(true);
+
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = Config::SMTP_HOST;                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = Config::SMTP_USER;                     //SMTP username
+    $mail->Password   = Config::SMTP_PASSWORD;                              //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = Config::SMTP_PORT;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('mahbubhussaincse@gmail.com', 'Mahbub');// from which mail sent this main
+    $mail->addAddress('devmahbubmamun@gmail.com', 'Mahbub Hussain User');     //Add a recipient
+    $mail->Body = 'This is a test message';
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('mahbubhussaincse@gmail.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+  
+
+$mail->send();
+    echo 'Message has been sent';
+}
+catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+
+
---
+++ mailer/src/Config/config.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mahbub\Phpmailer\Config;
+
+class Config
+{
+
+    /**
+     * SMTP host
+     *
+     * @var string
+     */
+    //const SMTP_HOST = 'sandbox.smtp.mailtrap.io';
+    const SMTP_HOST = 'smtp.gmail.com';
+
+    /**
+     * SMTP port
+     *
+     * @var int
+     */
+    //const SMTP_PORT = 2525;
+    const SMTP_PORT = 465;
+
+    /**
+     * SMTP user
+     *
+     * @var string
+     */
+    const SMTP_USER = 'mahbubhussaincse@gmail.com';
+    //const SMTP_USER = '69c6152cde6b19';
+
+    /**
+     * SMTP password
+     *
+     * @var string
+     */
+   // const SMTP_PASSWORD = 'b37999688aa343';
+     const SMTP_PASSWORD = 'uhwqwaqpgpuapwic';
+}
+
